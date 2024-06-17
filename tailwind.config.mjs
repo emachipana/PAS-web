0d6ad2aabbdea2d8a7dc989c24048d24bac37180
emachipana <emachipana0102@gmail.com>
2024-06-16 22:44:32 -0500

    ADD: custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        primary: "#1161AC",
+        secondary: "#FE6500"
+      }
+    },
 	},
 	plugins: [],
 }
